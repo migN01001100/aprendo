2885265c370d4999138f0d0c1c657c13ddd24874
Miguel Gonzalez <mga8919@gmail.com>
2021-12-12 10:13:21 +0200

    fixed some drawer styles

--- src/drawerSection.js
+++ src/drawerSection.js
@@ -27,16 +27,16 @@ export const ItemsDrawer = props => {
             <Caption style={styles.name}>@CustomUser</Caption>
             <Text style={styles.titleName}>Words:</Text>
             <PaperDrawer.Section>
-            <Drawer.Section style={styles.row}>
-                <Drawer.Section style={styles.section}>
-                    <Paragraph style={styles.paragraph}>{wordsLearnt}%</Paragraph>
-                    <Caption style={styles.caption}>Learnt</Caption>
+                <Drawer.Section style={styles.row}>
+                    <Drawer.Section style={styles.section}>
+                        <Paragraph style={styles.paragraph}>{wordsLearnt}%</Paragraph>
+                        <Caption style={styles.caption}>Learnt</Caption>
+                    </Drawer.Section>
+                    <Drawer.Section style={styles.section}>
+                        <Paragraph style={styles.paragraph}>{wordsSaved}</Paragraph>
+                        <Caption style={styles.caption}>Saved</Caption>
+                    </Drawer.Section>
                 </Drawer.Section>
-                <Drawer.Section style={styles.section}>
-                    <Paragraph style={styles.paragraph}>{wordsSaved}</Paragraph>
-                    <Caption style={styles.caption}>Saved</Caption>
-                </Drawer.Section>
-            </Drawer.Section>
             </PaperDrawer.Section>
             <PaperDrawer.Section style={styles.drawerSection}>
                 <PaperDrawer.Item icon="earth" label="Language" onPress={()=>{props.navigation.navigate('Language')}}/>
@@ -45,7 +45,7 @@ export const ItemsDrawer = props => {
                 <PaperDrawer.Item icon="bookshelf" label="Learnt" onPress={()=>props.navigation.navigate('Learnt')}/>
                 <PaperDrawer.Item icon="information-outline" label="About" onPress={()=>{}}/>
             </PaperDrawer.Section>
-                    <PaperDrawer.Item style={styles.bottomSection} icon="location-exit" label="Sign Out" onPress={()=>signOut()}/>
+                <PaperDrawer.Item style={styles.bottomSection} icon="location-exit" label="Sign Out" onPress={()=>signOut()}/>
         </PaperDrawer.Section>        
     )};
 const styles = StyleSheet.create({
